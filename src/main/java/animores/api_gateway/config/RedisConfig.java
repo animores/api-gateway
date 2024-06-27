7cde1a711057eee3214de94a98e586a9a082a9da
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2024-06-27 22:02:18 +0900

    feat: edit redis config

--- src/main/java/animores/api_gateway/config/RedisConfig.java
+++ src/main/java/animores/api_gateway/config/RedisConfig.java
@@ -1,9 +1,12 @@
 package animores.api_gateway.config;
 
 import animores.api_gateway.security.BlackListToken;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.ReactiveValueOperations;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -12,6 +15,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    @Primary
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
 
     @Bean
     ReactiveValueOperations<String, BlackListToken> redisOperations(ReactiveRedisConnectionFactory factory) {
